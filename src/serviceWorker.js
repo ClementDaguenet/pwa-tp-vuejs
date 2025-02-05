9740a8c3bfba82828384c6cbebd6972994fbd279
Clément Daguenet <clement.daguenet@microtec.fr>
2025-02-05 08:59:26 +0100

    Add Service Worker

---
+++ src/serviceWorker.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", (event) => {
+    console.log("[Service Worker] Installing Service Worker...");
+  
+    event.waitUntil(
+      caches.open("pwa-cache").then((cache) => {
+        return cache.addAll([
+          "/",
+          "/index.html",
+          "/favicon.ico",
+          "/manifest.json",
+          "/img/icons/android-chrome-192x192.png",
+          "/img/icons/android-chrome-512x512.png",
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("activate", (event) => {
+    console.log("[Service Worker] Activating Service Worker...");
+    event.waitUntil(self.clients.claim());
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "src/service-worker.js", // Utiliser notre propre Service Worker
+      exclude: ["_redirects"], // Éviter les conflits
+    },
+  },
+};
