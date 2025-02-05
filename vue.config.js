db41e4eea4d3cadd0dfe1ab3b60eb9d05f8f588f
Clément Daguenet <clement.daguenet@microtec.fr>
2025-02-05 09:16:06 +0100

    SW Correction

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
   pwa: {
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: "src/service-worker.js",
+      swSrc: "src/serviceWorker.js",
       swDest: "service-worker.js",
       exclude: ["_redirects"],
     },
   },
-};
+});
